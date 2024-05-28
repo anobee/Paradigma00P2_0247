db9969c1e15e200da25ca7aa773c95ee3c551a98
anobee <HP@DESKTOP-KFAVCD8>
2024-05-28 15:30:05 +0700

    menyelesaikan no pewarisan

--- noPewarisan.cpp
+++ noPewarisan.cpp
@@ -7,3 +7,17 @@ class baseclass final{
         cout << "halo saya function dari base class";
     }
 };
+
+class derivedclass : public  baseclass {
+    public :
+    void perkenalan () {
+        cout << "halo saya function dari derived class";
+    }
+};
+
+int main () {
+    derivedclass a;
+    a. perkenalan ();
+
+    return 0
+}
