a956531d45ac5782f450be54e95a2d0bd5bae1c1
anobee <HP@DESKTOP-KFAVCD8>
2024-05-28 14:52:06 +0700

    membuat class orang

---
+++ solveMultiwaris.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+class orang {
+    public:
+     int umur ;
+
+     orang (int pumur) :
+     umur (pumur)
+
+     {
+        cout << "orag dibuat dengan umur" << "\n" << endl;
+     }
+};
+
+class pekerja : public orang {
+   public :
+   pekerja (int pumur) :
+    orang (pumur) 
+    {
+      cout 
+    }
+}
